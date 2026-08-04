9e188ba79e2379c2919d1401bd868337f1a30bdd
Vishwas Garg <vishwasgarg.ml@gmail.com>
2026-08-04 23:23:14 +0530

    Update model_manager.py

--- model_manager.py
+++ model_manager.py
@@ -122,34 +122,34 @@ def backup_current_models():
                 archive_dir
             )
 
-    def replace_models(new_model_dir):
+    print(
+        f"Models backed up: {archive_dir}"
+    )
 
-        backup_current_models()
-    
-        for file in os.listdir(new_model_dir):
-    
-            source = os.path.join(
-                new_model_dir,
-                file
-            )
-    
-            destination = os.path.join(
-                MODEL_DIR,
-                file
-            )
-    
-            if os.path.isfile(source):
-    
-                shutil.copy(
-                    source,
-                    destination
-                )
-    
-        print(
-            "Production models replaced successfully."
+
+def replace_models(new_model_dir):
+
+    backup_current_models()
+
+    for file in os.listdir(new_model_dir):
+
+        source = os.path.join(
+            new_model_dir,
+            file
         )
-    
-    
-        print(
-            f"Models backed up: {archive_dir}"
+
+        destination = os.path.join(
+            MODEL_DIR,
+            file
         )
+
+        if os.path.isfile(source):
+
+            shutil.copy(
+                source,
+                destination
+            )
+
+    print(
+        "Production models replaced successfully."
+    )
