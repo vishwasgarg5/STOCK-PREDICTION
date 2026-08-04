e6ce4534ff4865229a85ce3d86716d660195c0d0
Vishwas Garg <vishwasgarg.ml@gmail.com>
2026-08-04 22:43:43 +0530

    Update test_accuracy.py

--- test_accuracy.py
+++ test_accuracy.py
@@ -1,8 +1,19 @@
-from accuracy_tracker import get_actual_price
+from accuracy_tracker import (
+    get_actual_price,
+    evaluate_prediction
+)
 
-result = get_actual_price(
+predicted = {
+    "Open": 554.90,
+    "High": 573.28,
+    "Low": 547.86,
+    "Close": 549.86
+}
+
+actual = get_actual_price(
     "AIIL",
     "2026-08-04"
 )
 
-print(result)
+print(actual)
+print(evaluate_prediction(predicted, actual))
