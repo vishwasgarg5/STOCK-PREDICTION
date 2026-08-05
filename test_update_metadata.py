743103c9629d7c106b3136d6f5a500ee3244c7a6
Vishwas Garg <vishwasgarg.ml@gmail.com>
2026-08-05 19:53:18 +0530

    Create test_update_metadata.py

---
+++ test_update_metadata.py
@@ -0,0 +1,10 @@
+from model_manager import update_metadata
+
+update_metadata({
+    "open": 5.20,
+    "high": 6.15,
+    "low": 4.80,
+    "close": 5.60
+})
+
+print("Metadata updated successfully.")
